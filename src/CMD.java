df3425a2ba303016598e65e9daf7eb75bfe36cc0
Mohamed Ihsan <89512452+pmihsan@users.noreply.github.com>
2023-06-10 21:30:21 +0530

    Added cd command functionality

--- src/CMD.java
+++ src/CMD.java
@@ -5,20 +5,24 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class CMD {
+    public static boolean start = true;
     public static void handleSocket(String[] cmd, DataInputStream in, DataOutputStream out, File dir) throws IOException{
         if(cmd[0].equalsIgnoreCase("list")){
-            listDirectory(out, dir);
+            listDirectory(out, returnDirectory(dir));
         }
         else if(cmd[0].equalsIgnoreCase("get")){
             for(int i=1;i<cmd.length;i++)
-                FTP.send(cmd[i], out, dir.getCanonicalPath());
+                FTP.send(cmd[i], out, returnDirectory(dir));
         }
         else if(cmd[0].equalsIgnoreCase("put")){
             for(int i=1;i<cmd.length;i++)
-                FTP.receive(cmd[i], in, dir.getCanonicalPath());
+                FTP.receive(cmd[i], in, returnDirectory(dir));
+        }
+        else if(cmd[0].equalsIgnoreCase("cd")){
+            changeDirectory(cmd, out, dir);
         }
         else if(cmd[0].equalsIgnoreCase("pwd")){
-            out.writeUTF(dir.getCanonicalPath());
+            out.writeUTF(returnDirectory(dir));
         }
         else if(cmd[0].equalsIgnoreCase("whoami")){
             out.writeUTF(System.getProperty("user.name"));
@@ -31,12 +35,43 @@ public class CMD {
         }
     }
 
-    public static void listDirectory(DataOutputStream out, File d) throws IOException {
-        String[] dirs = d.list();
+    public static void listDirectory(DataOutputStream out, String d) throws IOException {
+        File dir = new File(d);
+        String[] dirs = dir.list();
         StringBuilder res = new StringBuilder();
         for(int i = 0; i< Objects.requireNonNull(dirs).length; i++){
             res.append(dirs[i]).append("\t");
         }
         out.writeUTF(res.toString());
+    }
+
+    public static void changeDirectory(String[] cmd, DataOutputStream out, File dir) throws IOException{
+        if(cmd.length > 1 && cmd[1] != null) {
+            if (cmd[1].equals("..")) {
+                String cwd = returnDirectory(dir);
+                String path = cwd.substring(0, cwd.lastIndexOf(HELPER.path_sep));
+                if(path.contains("jftp" + HELPER.path_sep + "share")) {
+                    start = false;
+                    System.setProperty("user.dir", path);
+                    out.writeUTF("Current directory " + path);
+                }
+                else out.writeUTF("Unable to change directory");
+                return;
+            }
+            String path = dir.getAbsolutePath() + HELPER.path_sep + cmd[1];
+            File fp = new File(path);
+            if (fp.exists() && fp.isDirectory()) {
+                start = false;
+                System.setProperty("user.dir", fp.getAbsolutePath());
+                out.writeUTF("Directory changed to " + cmd[1]);
+            }
+            else out.writeUTF("Directory Invalid");
+        }
+        else out.writeUTF("Unable to change directory");
+    }
+
+    public static String returnDirectory(File dir){
+        if(start)return dir.getAbsolutePath();
+        else return System.getProperty("user.dir");
     }
 }
